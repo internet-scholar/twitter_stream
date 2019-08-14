27ab78890a40f11f441f8ee9159509ddc4b70a9e
Alexandre Goncalves <alexandre.reportagem@gmail.com>
2019-08-14 15:13:31 -0400

    several tweaks

--- init_script.sh
+++ init_script.sh
@@ -7,10 +7,8 @@ mkdir tmp
 wget http://repo1.maven.org/maven2/org/apache/orc/orc-tools/1.5.6/orc-tools-1.5.6-uber.jar -P ./tmp/
 wget https://raw.githubusercontent.com/internet-scholar/twitter_stream/master/requirements.txt
 wget https://raw.githubusercontent.com/internet-scholar/twitter_stream/master/twitter_stream.py
+wget https://raw.githubusercontent.com/internet-scholar/internet_scholar/master/requirements.txt -O requirements2.txt
 wget https://raw.githubusercontent.com/internet-scholar/internet_scholar/master/internet_scholar.py
-wget https://raw.githubusercontent.com/internet-scholar/twitter_stream/master/requirements.txt -O requirements2.txt
 pip3 install --trusted-host pypi.python.org -r /home/ubuntu/requirements.txt
 pip3 install --trusted-host pypi.python.org -r /home/ubuntu/requirements2.txt
-mkdir /home/ubuntu/.aws
-printf "[default]\\nregion={region}" > /home/ubuntu/.aws/config
-#python3 /home/ubuntu/twitter_stream.py -c $1 && sudo shutdown -h now
+python3 /home/ubuntu/twitter_stream.py -c $1 && sudo shutdown -h now
