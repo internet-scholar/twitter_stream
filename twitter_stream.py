3b4a3449d51ec06867b6929dd7366c6712415cbd
Alexandre Goncalves <alexandre.reportagem@gmail.com>
2019-08-16 14:58:45 -0400

    typo init_script.sh

--- twitter_stream.py
+++ twitter_stream.py
@@ -81,7 +81,7 @@ class TwitterStream:
         logging.info('Create TwitterStream object')
 
         # The default options for the time frame are below... in case they are changed for debugging purposes
-        start_saving_time = time(hour=18, minute=53, second=0, microsecond=0)
+        start_saving_time = time(hour=19, minute=3, second=0, microsecond=0)
         duration_saving = timedelta(minutes=1)
         delay_end = timedelta(seconds=30)
         # start_saving_time = time(hour=0, minute=0, second=0, microsecond=0)
